4a6f3a05e7fca3eb987faa0eb4702fccabf68cf2
Roland König <roland.koenig@rolandk.de>
2015-03-18 08:27:26 +0100

    Start development of a new Sample: VirtualTestingRoom

---
+++ Samples/Specialized/FrozenSky.Samples.VirtualTestRoom/MainWindow.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FrozenSky.Samples.VirtualTestRoom
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.m_ctrlRenderer = new FrozenSky.Multimedia.Views.FrozenSkyRendererControl();
+            this.m_dataSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.m_dataSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // m_ctrlRenderer
+            // 
+            this.m_ctrlRenderer.BackColor = System.Drawing.Color.LightBlue;
+            this.m_ctrlRenderer.Camera = null;
+            this.m_ctrlRenderer.DiscardRendering = false;
+            this.m_ctrlRenderer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.m_ctrlRenderer.Location = new System.Drawing.Point(0, 0);
+            this.m_ctrlRenderer.Name = "m_ctrlRenderer";
+            this.m_ctrlRenderer.Scene = null;
+            this.m_ctrlRenderer.Size = new System.Drawing.Size(792, 398);
+            this.m_ctrlRenderer.TabIndex = 0;
+            this.m_ctrlRenderer.Text = "frozenSkyRendererControl1";
+            this.m_ctrlRenderer.ViewConfiguration.AccentuationFactor = 0F;
+            this.m_ctrlRenderer.ViewConfiguration.AmbientFactor = 0.2F;
+            this.m_ctrlRenderer.ViewConfiguration.AntialiasingEnabled = true;
+            this.m_ctrlRenderer.ViewConfiguration.AntialiasingQuality = FrozenSky.Multimedia.Core.AntialiasingQualityLevel.Medium;
+            this.m_ctrlRenderer.ViewConfiguration.GeneratedBorderFactor = 1F;
+            this.m_ctrlRenderer.ViewConfiguration.GeneratedColorGradientFactor = 1F;
+            this.m_ctrlRenderer.ViewConfiguration.LightPower = 0.8F;
+            this.m_ctrlRenderer.ViewConfiguration.Overlay2DEnabled = true;
+            this.m_ctrlRenderer.ViewConfiguration.ShowTextures = true;
+            this.m_ctrlRenderer.ViewConfiguration.StrongLightFactor = 1.5F;
+            this.m_ctrlRenderer.ViewConfiguration.WireframeEnabled = false;
+            // 
+            // m_dataSource
+            // 
+            this.m_dataSource.DataSource = typeof(FrozenSky.Samples.VirtualTestRoom.TestingRoomViewModel);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 398);
+            this.Controls.Add(this.m_ctrlRenderer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "FrozenSky Samples - Virtual Test Room";
+            ((System.ComponentModel.ISupportInitialize)(this.m_dataSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Multimedia.Views.FrozenSkyRendererControl m_ctrlRenderer;
+        private System.Windows.Forms.BindingSource m_dataSource;
+    }
+}
+
---
+++ Samples/Specialized/FrozenSky.Samples.VirtualTestRoom/TestingRoomViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrozenSky;
+using FrozenSky.Util;
+using FrozenSky.Multimedia.Core;
+using FrozenSky.Multimedia.Drawing3D;
+using FrozenSky.Multimedia.Objects;
+
+namespace FrozenSky.Samples.VirtualTestRoom
+{
+    public class TestingRoomViewModel : ViewModelBase
+    {
+        private Scene m_scene;
+        private PerspectiveCamera3D m_camera;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestingRoomViewModel"/> class.
+        /// </summary>
+        public TestingRoomViewModel()
+        {
+            m_scene = new Scene();
+            m_scene.ManipulateSceneAsync(OnInitializeScene)
+                .FireAndForget();
+            m_camera = new PerspectiveCamera3D();
+
+            m_camera = new PerspectiveCamera3D();
+            m_camera.Position = new Vector3(0f, 2f, 0f);
+            m_camera.Target = new Vector3(0f, 1.5f, 2f);
+        }
+
+        /// <summary>
+        /// Called during the first update pass of the local scene object.
+        /// </summary>
+        /// <param name="manipulator">The object provided for scene manipulation.</param>
+        private void OnInitializeScene(SceneManipulator manipulator)
+        {
+            Grid3DType gridTypeTopCube = new Grid3DType();
+            gridTypeTopCube.GenerateGround = false;
+            gridTypeTopCube.TilesX = 20;
+            gridTypeTopCube.TilesZ = 20;
+            gridTypeTopCube.TileWidth = 1f;
+            gridTypeTopCube.GroupTileCount = 20;
+            gridTypeTopCube.LineColor = Color.DarkBlue;
+            var resGridTopCube = manipulator.AddGeometry(gridTypeTopCube);
+
+            Grid3DType gridTypeFloor = new Grid3DType();
+            gridTypeFloor.GenerateGround = false;
+            gridTypeFloor.TilesX = 100;
+            gridTypeFloor.TilesZ = 100;
+            gridTypeFloor.TileWidth = 0.2f;
+            gridTypeFloor.LineColor = Color.DarkBlue;
+            var resGridFloor = manipulator.AddGeometry(gridTypeFloor);
+
+            // Build floor
+            {
+                GenericObject objFloor = manipulator.AddGeneric(resGridFloor);
+            }
+
+            // Build top cube
+            {
+                GenericObject objFront = manipulator.AddGeneric(resGridTopCube);
+                objFront.Position = new Vector3(0f, 10f, 10f);
+                objFront.RotationEuler = new Vector3(-EngineMath.RAD_90DEG, 0f, 0f);
+
+                GenericObject objBack = manipulator.AddGeneric(resGridTopCube);
+                objBack.Position = new Vector3(0f, 10f, -10f);
+                objBack.RotationEuler = new Vector3(EngineMath.RAD_90DEG, 0f, 0f);
+
+                GenericObject objLeft = manipulator.AddGeneric(resGridTopCube);
+                objLeft.Position = new Vector3(-10f, 10f, 0f);
+                objLeft.RotationEuler = new Vector3(0f, 0f, -EngineMath.RAD_90DEG);
+            }
+
+        }
+
+        public Scene Scene
+        {
+            get { return m_scene; }
+        }
+
+        public Camera3DBase Camera
+        {
+            get { return m_camera; }
+        }
+    }
+}
