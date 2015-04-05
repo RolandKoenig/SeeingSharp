b0a270330b52cb26c79778c1c02d129886cd62dc
Roland König <roland.koenig@rolandk.de>
2015-04-05 08:37:58 +0200

    Changed structure of behavior folders

---
+++ FrozenSky/Behaviors/_Wpf/FrozenSkyPropertyGridBehavior.cs
@@ -0,0 +1,64 @@
+﻿#region License information (FrozenSky and all based games/applications)
+/*
+    FrozenSky and all games/applications based on it (more info at http://www.rolandk.de/wp)
+    Copyright (C) 2015 Roland König (RolandK)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+
+	********************************************************************************
+    Additional permission under GNU GPL version 3 section 7
+
+    If you modify this Program, or any covered work, by linking or combining it with 
+	DirectX (or a modified version of that library), containing parts covered by the 
+	terms of [name of library's license], the licensors of this Program grant you additional 
+	permission to convey the resulting work. {Corresponding Source for a non-source form of 
+	such a combination shall include the source code for the parts of DirectX used 
+	as well as that of the covered work.}
+*/
+#endregion
+#if DESKTOP
+using FrozenSky.Util;
+using PropertyTools.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interactivity;
+
+namespace FrozenSky.Behaviors
+{
+    public class FrozenSkyPropertyGridBehavior : Behavior<PropertyGrid>
+    {
+        /// <summary>
+        /// Wird nach dem Anfügen des Verhaltens an das "AssociatedObject" aufgerufen.
+        /// </summary>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            base.AssociatedObject.PropertyControlFactory = new FrozenSkyPropertyControlFactory();
+            base.AssociatedObject.PropertyItemFactory = new FrozenSkyPropertyItemFactory();
+        }
+
+        /// <summary>
+        /// Wird aufgerufen, wenn das Verhalten vom "AssociatedObject" getrennt wird. Der Aufruf erfolgt vor dem eigentlichen Trennvorgang.
+        /// </summary>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+        }
+    }
+}
+#endif
