d5b3435f44ad7babcf04973f24058586c21b00c8
Roland König <roland.koenig@rolandk.de>
2015-04-15 06:56:46 +0200

    Refactoring of the sample container

---
+++ Samples/FrozenSky.Samples.Base/_Base/SampleBase.cs
@@ -0,0 +1,50 @@
+﻿#region License information (FrozenSky and all based games/applications)
+/*
+    FrozenSky and all games/applications based on it (more info at http://www.rolandk.de/wp)
+    Copyright (C) 2015 Roland König (RolandK)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+#endregion
+using FrozenSky.Multimedia.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrozenSky.Samples.Base
+{
+    abstract class SampleBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SampleBase"/> class.
+        /// </summary>
+        public SampleBase()
+        {
+
+        }
+
+        /// <summary>
+        /// Called when the sample has to startup.
+        /// </summary>
+        /// <param name="targetRenderLoop">The target render loop.</param>
+        public abstract Task OnStartup(RenderLoop targetRenderLoop);
+
+        /// <summary>
+        /// Called when the sample has to run down.
+        /// </summary>
+        public abstract Task OnClose();
+    }
+}
