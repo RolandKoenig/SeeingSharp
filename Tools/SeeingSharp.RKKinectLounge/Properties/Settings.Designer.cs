03b452ac4e0c099e7c4a7c66616d511a246975c3
Roland König <roland.koenig@rolandk.de>
2015-06-27 19:17:03 +0200

    KinectLounge: Neuer Home-Button

--- Tools/SeeingSharp.RKKinectLounge/Properties/Settings.Designer.cs
+++ Tools/SeeingSharp.RKKinectLounge/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace SeeingSharp.RKKinectLounge.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\_RKKinectLounge")]
+        [global::System.Configuration.DefaultSettingValueAttribute("D:\\SkyDrive\\RKKinectLounge")]
         public string DataPath {
             get {
                 return ((string)(this["DataPath"]));
