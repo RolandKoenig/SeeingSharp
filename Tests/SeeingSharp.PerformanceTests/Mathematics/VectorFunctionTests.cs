b386bbc867178e87fba36f6e54e404e5bdf5ae9d
Roland König <roland.koenig@rolandk.de>
2015-07-24 05:52:55 +0200

    Common: Simple performance comparison between SeeingSharp.Vector3 and System.Numerics.Vector3

---
+++ Tests/SeeingSharp.PerformanceTests/Mathematics/VectorFunctionTests.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeingSharp.PerformanceTests.Mathematics
+{
+    public static class VectorFunctionTests
+    {
+        #region SeeingSharp
+        public static TimeSpan Check_SeeingSharp_Vector_Multiplication()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            {
+                Vector3 dummyVector = new Vector3(2f, 3f, 4f);
+                Vector3 dummyVector2 = new Vector3(1f, 2f, 3f);
+
+                Vector3 resultVector = Vector3.Zero;
+                for(int loop=0; loop<100000; loop++)
+                {
+                    resultVector = dummyVector * dummyVector2;
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public static TimeSpan Check_SeeingSharp_Vector_Add()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            {
+                Vector3 dummyVector = new Vector3(2f, 3f, 4f);
+                Vector3 dummyVector2 = new Vector3(1f, 2f, 3f);
+
+                Vector3 resultVector = Vector3.Zero;
+                for (int loop = 0; loop < 100000; loop++)
+                {
+                    resultVector = dummyVector + dummyVector2;
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public static TimeSpan Check_SeeingSharp_Vector_Subtract()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            {
+                Vector3 dummyVector = new Vector3(2f, 3f, 4f);
+                Vector3 dummyVector2 = new Vector3(1f, 2f, 3f);
+
+                Vector3 resultVector = Vector3.Zero;
+                for (int loop = 0; loop < 100000; loop++)
+                {
+                    resultVector = dummyVector - dummyVector2;
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public static TimeSpan Check_SeeingSharp_Vector_Transform()
+        {
+            Matrix4x4 transformMatrix = Matrix4x4.RotationYawPitchRoll(1f, 1f, 1f);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            {
+                Vector3 dummyVector = new Vector3(2f, 3f, 4f);
+                Vector3 dummyVector2 = new Vector3(1f, 2f, 3f);
+
+                Vector3 resultVector = Vector3.Zero;
+                for (int loop = 0; loop < 100000; loop++)
+                {
+                    resultVector = Vector3.Transform(dummyVector, transformMatrix).XYZ;
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+        #endregion
+        //*********************************************************************
+        //*********************************************************************
+        //*********************************************************************
+        #region System.Numerics
+        public static TimeSpan Check_SystemNumerics_Vector_Multiplication()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            {
+                System.Numerics.Vector3 dummyVector = new System.Numerics.Vector3(2f, 3f, 4f);
+                System.Numerics.Vector3 dummyVector2 = new System.Numerics.Vector3(1f, 2f, 3f);
+
+                System.Numerics.Vector3 resultVector = System.Numerics.Vector3.Zero;
+                for (int loop = 0; loop < 100000; loop++)
+                {
+                    resultVector = dummyVector * dummyVector2;
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public static TimeSpan Check_SystemNumerics_Vector_Add()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            {
+                System.Numerics.Vector3 dummyVector = new System.Numerics.Vector3(2f, 3f, 4f);
+                System.Numerics.Vector3 dummyVector2 = new System.Numerics.Vector3(1f, 2f, 3f);
+
+                System.Numerics.Vector3 resultVector = System.Numerics.Vector3.Zero;
+                for (int loop = 0; loop < 100000; loop++)
+                {
+                    resultVector = dummyVector + dummyVector2;
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public static TimeSpan Check_SystemNumerics_Vector_Subtract()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            {
+                System.Numerics.Vector3 dummyVector = new System.Numerics.Vector3(2f, 3f, 4f);
+                System.Numerics.Vector3 dummyVector2 = new System.Numerics.Vector3(1f, 2f, 3f);
+
+                System.Numerics.Vector3 resultVector = System.Numerics.Vector3.Zero;
+                for (int loop = 0; loop < 100000; loop++)
+                {
+                    resultVector = dummyVector - dummyVector2;
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public static TimeSpan Check_SystemNumerics_Vector_Transform()
+        {
+            Matrix4x4 matrix = Matrix4x4.RotationYawPitchRoll(1f, 1f, 1f);
+            System.Numerics.Matrix4x4 transformMatrix = new System.Numerics.Matrix4x4(
+                matrix.M11, matrix.M12, matrix.M13, matrix.M14,
+                matrix.M21, matrix.M22, matrix.M23, matrix.M24,
+                matrix.M31, matrix.M32, matrix.M33, matrix.M34,
+                matrix.M41, matrix.M42, matrix.M43, matrix.M44);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            {
+                System.Numerics.Vector3 dummyVector = new System.Numerics.Vector3(2f, 3f, 4f);
+                System.Numerics.Vector3 dummyVector2 = new System.Numerics.Vector3(1f, 2f, 3f);
+
+                System.Numerics.Vector3 resultVector = System.Numerics.Vector3.Zero;
+                for (int loop = 0; loop < 100000; loop++)
+                {
+                    resultVector = System.Numerics.Vector3.Transform(dummyVector, transformMatrix);
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+        #endregion
+    }
+}
---
+++ Tests/SeeingSharp.PerformanceTests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeeingSharp.PerformanceTests.Mathematics;
+
+namespace SeeingSharp.PerformanceTests
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("#################### SeeingSharp Vector");
+            Console.WriteLine("Multiplication: " + VectorFunctionTests.Check_SeeingSharp_Vector_Multiplication().TotalMilliseconds.ToString("F2") + "ms");
+            Console.WriteLine("Add:            " + VectorFunctionTests.Check_SeeingSharp_Vector_Add().TotalMilliseconds.ToString("F2") + "ms");
+            Console.WriteLine("Subtract:       " + VectorFunctionTests.Check_SeeingSharp_Vector_Subtract().TotalMilliseconds.ToString("F2") + "ms");
+            Console.WriteLine("Transform:      " + VectorFunctionTests.Check_SeeingSharp_Vector_Transform().TotalMilliseconds.ToString("F2") + "ms");
+            Console.WriteLine();
+
+            Console.WriteLine("#################### System.Numerics Vector");
+            Console.WriteLine("Multiplication: " + VectorFunctionTests.Check_SystemNumerics_Vector_Multiplication().TotalMilliseconds.ToString("F2") + "ms");
+            Console.WriteLine("Add:            " + VectorFunctionTests.Check_SystemNumerics_Vector_Add().TotalMilliseconds.ToString("F2") + "ms");
+            Console.WriteLine("Subtract:       " + VectorFunctionTests.Check_SystemNumerics_Vector_Subtract().TotalMilliseconds.ToString("F2") + "ms");
+            Console.WriteLine("Transform:      " + VectorFunctionTests.Check_SystemNumerics_Vector_Transform().TotalMilliseconds.ToString("F2") + "ms");
+            Console.WriteLine();
+        }
+    }
+}
