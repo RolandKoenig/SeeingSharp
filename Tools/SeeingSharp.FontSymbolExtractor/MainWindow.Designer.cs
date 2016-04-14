45755f48b5f94c9327eeec4deca8a624e359f000
Roland König <roland.koenig@rolandk.de>
2016-04-14 07:01:04 +0200

    Seeing#: Simple font -> png converter

--- Tools/SeeingSharp.FontSymbolExtractor/MainWindow.Designer.cs
+++ Tools/SeeingSharp.FontSymbolExtractor/MainWindow.Designer.cs
@@ -35,19 +35,30 @@
             this.m_lblSize = new System.Windows.Forms.Label();
             this.m_lblSizeX = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.m_menu = new System.Windows.Forms.MenuStrip();
             this.m_cmdConvert = new System.Windows.Forms.Button();
+            this.m_menu = new System.Windows.Forms.MenuStrip();
             this.m_dlgSelectTargetFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.m_txtOutputDirectory = new System.Windows.Forms.TextBox();
+            this.m_lblOutputDirectory = new System.Windows.Forms.Label();
+            this.m_cmdOutputDirectory = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.m_lblCharacters = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // m_cmbFont
             // 
+            this.m_cmbFont.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.m_cmbFont.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.m_cmbFont.FormattingEnabled = true;
-            this.m_cmbFont.Location = new System.Drawing.Point(129, 3);
+            this.m_cmbFont.Location = new System.Drawing.Point(165, 3);
             this.m_cmbFont.Name = "m_cmbFont";
-            this.m_cmbFont.Size = new System.Drawing.Size(191, 24);
+            this.m_cmbFont.Size = new System.Drawing.Size(436, 24);
             this.m_cmbFont.TabIndex = 0;
             // 
             // m_lblFont
@@ -61,31 +72,33 @@
             // 
             // m_txtSizeWidth
             // 
-            this.m_txtSizeWidth.Location = new System.Drawing.Point(129, 33);
+            this.m_txtSizeWidth.Location = new System.Drawing.Point(165, 89);
             this.m_txtSizeWidth.Name = "m_txtSizeWidth";
             this.m_txtSizeWidth.Size = new System.Drawing.Size(46, 22);
             this.m_txtSizeWidth.TabIndex = 2;
+            this.m_txtSizeWidth.Text = "32";
             // 
             // m_txtSizeHeight
             // 
-            this.m_txtSizeHeight.Location = new System.Drawing.Point(201, 33);
+            this.m_txtSizeHeight.Location = new System.Drawing.Point(237, 89);
             this.m_txtSizeHeight.Name = "m_txtSizeHeight";
             this.m_txtSizeHeight.Size = new System.Drawing.Size(46, 22);
             this.m_txtSizeHeight.TabIndex = 3;
+            this.m_txtSizeHeight.Text = "32";
             // 
             // m_lblSize
             // 
             this.m_lblSize.AutoSize = true;
-            this.m_lblSize.Location = new System.Drawing.Point(2, 36);
+            this.m_lblSize.Location = new System.Drawing.Point(2, 92);
             this.m_lblSize.Name = "m_lblSize";
-            this.m_lblSize.Size = new System.Drawing.Size(72, 17);
+            this.m_lblSize.Size = new System.Drawing.Size(114, 17);
             this.m_lblSize.TabIndex = 4;
-            this.m_lblSize.Text = "Pixel Size:";
+            this.m_lblSize.Text = "Output Size (px):";
             // 
             // m_lblSizeX
             // 
             this.m_lblSizeX.AutoSize = true;
-            this.m_lblSizeX.Location = new System.Drawing.Point(181, 36);
+            this.m_lblSizeX.Location = new System.Drawing.Point(217, 92);
             this.m_lblSizeX.Name = "m_lblSizeX";
             this.m_lblSizeX.Size = new System.Drawing.Size(14, 17);
             this.m_lblSizeX.TabIndex = 5;
@@ -93,6 +106,15 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.m_lblCharacters);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.m_cmdOutputDirectory);
+            this.panel1.Controls.Add(this.m_lblOutputDirectory);
+            this.panel1.Controls.Add(this.m_txtOutputDirectory);
             this.panel1.Controls.Add(this.m_cmdConvert);
             this.panel1.Controls.Add(this.m_lblFont);
             this.panel1.Controls.Add(this.m_lblSizeX);
@@ -106,6 +128,16 @@
             this.panel1.Size = new System.Drawing.Size(604, 304);
             this.panel1.TabIndex = 6;
             // 
+            // m_cmdConvert
+            // 
+            this.m_cmdConvert.Location = new System.Drawing.Point(165, 179);
+            this.m_cmdConvert.Name = "m_cmdConvert";
+            this.m_cmdConvert.Size = new System.Drawing.Size(136, 29);
+            this.m_cmdConvert.TabIndex = 6;
+            this.m_cmdConvert.Text = "Convert!";
+            this.m_cmdConvert.UseVisualStyleBackColor = true;
+            this.m_cmdConvert.Click += new System.EventHandler(this.OnCmdConvert_Click);
+            // 
             // m_menu
             // 
             this.m_menu.ImageScalingSize = new System.Drawing.Size(20, 20);
@@ -115,15 +147,81 @@
             this.m_menu.TabIndex = 7;
             this.m_menu.Text = "menuStrip1";
             // 
-            // m_cmdConvert
+            // m_txtOutputDirectory
             // 
-            this.m_cmdConvert.Location = new System.Drawing.Point(129, 86);
-            this.m_cmdConvert.Name = "m_cmdConvert";
-            this.m_cmdConvert.Size = new System.Drawing.Size(136, 29);
-            this.m_cmdConvert.TabIndex = 6;
-            this.m_cmdConvert.Text = "Convert!";
-            this.m_cmdConvert.UseVisualStyleBackColor = true;
-            this.m_cmdConvert.Click += new System.EventHandler(this.OnCmdConvert_Click);
+            this.m_txtOutputDirectory.Location = new System.Drawing.Point(165, 33);
+            this.m_txtOutputDirectory.Name = "m_txtOutputDirectory";
+            this.m_txtOutputDirectory.Size = new System.Drawing.Size(399, 22);
+            this.m_txtOutputDirectory.TabIndex = 7;
+            // 
+            // m_lblOutputDirectory
+            // 
+            this.m_lblOutputDirectory.AutoSize = true;
+            this.m_lblOutputDirectory.Location = new System.Drawing.Point(2, 36);
+            this.m_lblOutputDirectory.Name = "m_lblOutputDirectory";
+            this.m_lblOutputDirectory.Size = new System.Drawing.Size(116, 17);
+            this.m_lblOutputDirectory.TabIndex = 8;
+            this.m_lblOutputDirectory.Text = "Output Directory:";
+            // 
+            // m_cmdOutputDirectory
+            // 
+            this.m_cmdOutputDirectory.Location = new System.Drawing.Point(570, 33);
+            this.m_cmdOutputDirectory.Name = "m_cmdOutputDirectory";
+            this.m_cmdOutputDirectory.Size = new System.Drawing.Size(31, 23);
+            this.m_cmdOutputDirectory.TabIndex = 9;
+            this.m_cmdOutputDirectory.Text = "...";
+            this.m_cmdOutputDirectory.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 22);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "E000";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(289, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 22);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "E21D";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "..to..";
+            // 
+            // m_lblCharacters
+            // 
+            this.m_lblCharacters.AutoSize = true;
+            this.m_lblCharacters.Location = new System.Drawing.Point(3, 64);
+            this.m_lblCharacters.Name = "m_lblCharacters";
+            this.m_lblCharacters.Size = new System.Drawing.Size(123, 17);
+            this.m_lblCharacters.TabIndex = 13;
+            this.m_lblCharacters.Text = "Characters (HEX):";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(165, 117);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(46, 22);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "16";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Font Size (pt):";
             // 
             // MainWindow
             // 
@@ -154,6 +252,15 @@
         private System.Windows.Forms.Button m_cmdConvert;
         private System.Windows.Forms.MenuStrip m_menu;
         private System.Windows.Forms.FolderBrowserDialog m_dlgSelectTargetFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label m_lblCharacters;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button m_cmdOutputDirectory;
+        private System.Windows.Forms.Label m_lblOutputDirectory;
+        private System.Windows.Forms.TextBox m_txtOutputDirectory;
     }
 }
 
--- Tools/SeeingSharp.FontSymbolExtractor/MainWindow.cs
+++ Tools/SeeingSharp.FontSymbolExtractor/MainWindow.cs
@@ -1,10 +1,10 @@
-﻿using SeeingSharp.Multimedia.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +24,47 @@ namespace SeeingSharp.FontSymbolExtractor
             base.OnLoad(e);
 
             m_cmbFont.Items.Clear();
-            m_cmbFont.Items.AddRange(GraphicsCore.Current.GetFontFamilyNames(CultureInfo.CurrentCulture.Name).ToArray());
+            m_cmbFont.Items.AddRange(FontFamily.Families);
         }
 
         private void OnCmdConvert_Click(object sender, EventArgs e)
         {
+            FontFamily selectedFamily = m_cmbFont.SelectedItem as FontFamily;
+            if(selectedFamily == null) { return; }
+
+            int bitmapWidth = 0;
+            int bitmapHeight = 0;
+            if(!Int32.TryParse(m_txtSizeWidth.Text, out bitmapWidth)) { return; }
+            if(!Int32.TryParse(m_txtSizeHeight.Text, out bitmapHeight)) { return; }
+
+            if(m_dlgSelectTargetFolder.ShowDialog(this) == DialogResult.OK)
+            {
+                string targetDir = m_dlgSelectTargetFolder.SelectedPath;
+
+                using (SolidBrush foreBrush = new SolidBrush(System.Drawing.Color.Black))
+                using (SolidBrush backBrush = new SolidBrush(System.Drawing.Color.Transparent))
+                using (Bitmap targetBitmap = new Bitmap(bitmapWidth, bitmapHeight))
+                using (Graphics bitmapGraphics = Graphics.FromImage(targetBitmap))
+                using (Font newFont = new Font(selectedFamily, 16f))
+                {
+                    bitmapGraphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                    bitmapGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bilinear;
+                    bitmapGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    bitmapGraphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                    for (ushort loopChar = 0xE000; loopChar < 0xE21E; loopChar++)
+                    {
+                        bitmapGraphics.Clear(System.Drawing.Color.Transparent);
+                        bitmapGraphics.DrawString(
+                            Encoding.Unicode.GetString(BitConverter.GetBytes(loopChar)),
+                            newFont,
+                            foreBrush,
+                            new PointF(-1f, 1f));
+                        bitmapGraphics.Flush();
+
+                        targetBitmap.Save(Path.Combine(targetDir, "Icon_" + loopChar + ".png"));
+                    }
+                }
+            }
 
         }
     }
