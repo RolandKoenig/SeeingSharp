895c1d51c736d7fc1febeefafdd689c90150ae3b
Roland König <roland.koenig@rolandk.de>
2016-08-20 07:42:37 +0200

    Seeing#: Added test project for model viewer

---
+++ Tools/SeeingSharpModelViewer.Tests/ViewModelTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SeeingSharpModelViewer.Tests
+{
+    public class ViewModelTests
+    {
+        public const string TEST_CATEGORY = "SeeingSharp ModelViewer ViewModel";
+
+        [Fact]
+        [Trait("Category", TEST_CATEGORY)]
+        public void Check_Something()
+        {
+
+        }
+    }
+}
