79d7bdc9680a08e06c407bd835b9ce6605f224d4
RolandKoenig <roland.koenig@rkoenig.eu>
2017-10-28 06:54:21 +0200

    Revert "Seeing#: Removed android project"
    This reverts commit 80a33e148bfe71b0ea9dccb07ba6d12e6f6a18c1.

--- SeeingSharp.BuildTasks/ShaderBuildTask.cs
+++ SeeingSharp.BuildTasks/ShaderBuildTask.cs
@@ -53,13 +53,8 @@ namespace SeeingSharp.BuildTasks
                     as string;
                 if (string.IsNullOrEmpty(winSdkPath))
                 {
-                    winSdkPath = Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Microsoft SDKs\\Windows\\v10.0", "InstallationFolder", null)
-                        as string;
-                    if (string.IsNullOrEmpty(winSdkPath))
-                    {
-                        base.Log.LogError("Unable to compile shader: Windows SDK v8.1 or v10 64-Bit not found!");
-                        return false;
-                    }
+                    base.Log.LogError("Unable to compile shader: Windows SDK 8.1 64-Bit not found!");
+                    return false;
                 }
 
                 // Search fxc.exe
